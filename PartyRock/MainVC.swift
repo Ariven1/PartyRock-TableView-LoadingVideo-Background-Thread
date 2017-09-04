dc3c9c6df4f5fa3a2bd053463c80a7f120b967dd
Ariven <ariven@Arivens-Mac.local>
2017-09-05 00:53:04 +0530

    lesson01 - Designing UI

--- PartyRock/MainVC.swift
+++ PartyRock/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  PartyRock
 //
 //  Created by Ariven on 05/09/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
